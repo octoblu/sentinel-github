1145b3647a889ed7d8b8de0fff127ad6c40af3d3
sqrt <sqrt@octoblu.com>
2016-06-17 17:23:55 -0700

    Add base64 image on failure

--- command.js
+++ command.js
@@ -3,7 +3,9 @@
 var system = require('system');
 var casper = require('casper').create({
   onError: (function(error){
-    casper.echo("failure due to error: " + error)
+    console.log("failure due to error: " + error)
+    casper.capture('error.png')
+    console.log(this.echo(casper.captureBase64('png')))
     casper.exit(1)
   })
 });
@@ -11,7 +13,10 @@ var casper = require('casper').create({
 var GITHUB_USERNAME = system.env.GITHUB_USERNAME;
 var GITHUB_PASSWORD = system.env.GITHUB_PASSWORD;
 
-if(!GITHUB_USERNAME || !GITHUB_PASSWORD) casper.die('Missing required env: GITHUB_USERNAME or GITHUB_PASSWORD')
+if(!GITHUB_USERNAME || !GITHUB_PASSWORD) {
+  console.log('Missing required env: GITHUB_USERNAME or GITHUB_PASSWORD')
+  this.exit(1)
+}
 
 casper.start('https://app.octoblu.com/');
 
@@ -21,7 +26,7 @@ casper.then(function() {
   this.click('.auth__button--github');
 })
 
-casper.waitForText("Sign in to GitHub")
+casper.waitForSelector("#login")
 
 casper.then(function(){
   this.fill('#login', {
@@ -35,7 +40,10 @@ casper.waitForText("dashboard", function(){
   this.echo("success");
   this.exit()
 }, function(){
-  this.die("failure")
+  console.log("failure")
+  casper.capture('failure.png')
+  console.log(this.echo(casper.captureBase64('png')))
+  this.exit(1)
 })
 
 casper.run();
