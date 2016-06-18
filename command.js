c1e8004b1bfd59065eb63cd7770fef7e0c43d435
sqrt <sqrt@octoblu.com>
2016-06-17 17:37:27 -0700

    v1.0.4 Add Dockerfile

--- command.js
+++ command.js
@@ -4,11 +4,11 @@ var system = require('system');
 var casper = require('casper').create({
   onError: (function(error){
     console.log("failure due to error: " + error)
-    casper.capture('error.png')
     console.log(this.echo(casper.captureBase64('png')))
     casper.exit(1)
   })
 });
+
 
 var GITHUB_USERNAME = system.env.GITHUB_USERNAME;
 var GITHUB_PASSWORD = system.env.GITHUB_PASSWORD;
@@ -18,6 +18,7 @@ if(!GITHUB_USERNAME || !GITHUB_PASSWORD) {
   this.exit(1)
 }
 
+casper.userAgent('Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.84 Safari/537.36');
 casper.start('https://app.octoblu.com/');
 
 casper.waitForText("Github")
@@ -26,22 +27,25 @@ casper.then(function() {
   this.click('.auth__button--github');
 })
 
-casper.waitForSelector("#login")
+casper.then(function(){
+  casper.waitForSelector("#login")
+})
 
 casper.then(function(){
+  this.capture('pre-form.png')
   this.fill('#login', {
-    'login_field': GITHUB_USERNAME,
+    'login': GITHUB_USERNAME,
     'password': GITHUB_PASSWORD
   })
-  this.click("#commit")
+
+  this.click('input[name="commit"]')
 })
 
 casper.waitForText("dashboard", function(){
-  this.echo("success");
+  this.echo("success", 'INFO');
   this.exit()
 }, function(){
-  console.log("failure")
-  casper.capture('failure.png')
+  this.echo("failure", 'ERROR');
   console.log(this.echo(casper.captureBase64('png')))
   this.exit(1)
 })
